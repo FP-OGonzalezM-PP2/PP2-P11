5a5f3e8e0baadab5655710ee6a02dedd5dcb8cd3
Alumno <Alumno@328-26>
2016-10-20 12:09:12 -0500

    Numeros romanos

--- src/pp2/p11/PP2P11.java
+++ src/pp2/p11/PP2P11.java
@@ -21,35 +21,36 @@ public class PP2P11 {
         int n;
         String nR;
         n = pedirNumero();
-        nR = conversion(numeros,numeroRomanos,n);
+        nR = conversion(n,numeros,numeroRomanos);
         Mostrar(nR);
     }
     public static int pedirNumero(){
         int n;
         Scanner entrada = new Scanner(System.in);
         System.out.println("Escriba un el número que quiera convertir a romano ");
+        System.out.println("--------------------------------------------------");
         System.out.println("El número debe ser menor de 3999");
         n = entrada.nextInt();
         return n; 
     }
-    public static String conversion (int []numeros, String [] numerosRomanos, int n){
-        String nR = "";
-        int j=0,m=0;
+    public static String conversion (int n, int[] numeros, String[] numerosRomanos){
+        String nR="";   
+        int j = 0,i=0;
         {                                         
-            for (int i=numerosRomanos.length-1; i>=0; --i) 
-            {                                        
-                j = n / numeros[i];                  
-                while (m > 0)                  
-                {                                    
-                    nR = nR + numeros[i];
-                    m--;   
+            for (i=numeros.length-1; i>=0; --i){                                        
+                j= n / numeros[i];                  
+                while (j > 0){                                    
+                    nR = nR + numerosRomanos[i];
+                    j--;                        
                 }
-                n = n%numeros[i];                    
-    }
+                n = n%numeros[i];   
+            }
+        return nR;  
         }
-        return nR;
     }
+
     public static void Mostrar(String nR){
         System.out.println(nR);
     }
 }
+
